29e6a1d2531f87a530ae31066a24acfd53f6fded
Seo-JuHyeong <sjh990721@naver.com>
2023-01-04 15:47:07 +0900

    add Alert

--- src/database/sql.js
+++ src/database/sql.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool(process.env.JAWSDB_URL ?? {
     host: 'localhost',
     user: 'root',
     database: 'car_dealer',
-    password: '730216hj',
+    password: '',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
